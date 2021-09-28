311fa0b2fb09b51ddef410b4a4c19853f95bd56a
Dwhite77 <dan_white18@live.com>
2021-09-28 19:22:20 +0100

    got the javafx working after a few issues with imports

--- src/main/java/com/sparta/view/GUI.java
+++ src/main/java/com/sparta/view/GUI.java
@@ -1,15 +1,25 @@
 package com.sparta.view;
 
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
 //----------------------------------------------------------------------------------------------
 //  GUI stuff
 //----------------------------------------------------------------------------------------------
 
-public class GUI{
-
-
+public class GUI extends Application{
 
     public GUI() {
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
 
     }
 
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.week4migration {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
 
 
     opens com.example.week4migration to javafx.fxml;
