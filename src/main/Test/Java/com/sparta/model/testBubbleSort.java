ae4dec9bd10f1caf2b078824b033aaa5ea20635b
Dwhite77 <dan_white18@live.com>
2021-09-29 12:07:02 +0100

    added the initial files for the testing of the other sort types

---
+++ src/main/Test/Java/com/sparta/model/testBubbleSort.java
@@ -0,0 +1,13 @@
+package com.sparta.model;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class testBubbleSort {
+    @Test
+    public void testSort(){
+        BubbleSort myBubbleSort = new BubbleSort();
+//        myQuickSort.sort()
+//        assertTrue();
+    }
+}
---
+++ src/main/Test/Java/com/sparta/model/testMergeSort.java
@@ -0,0 +1,13 @@
+package com.sparta.model;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class testMergeSort {
+    @Test
+    public void testSort(){
+        MergeSort myMergeSort = new MergeSort();
+//        myQuickSort.sort()
+//        assertTrue();
+    }
+}
