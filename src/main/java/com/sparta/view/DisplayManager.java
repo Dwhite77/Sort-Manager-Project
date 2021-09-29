d510d52f8d1988cad9cc60ab23425e3b338cb1db
Dwhite77 <dan_white18@live.com>
2021-09-29 13:49:12 +0100

    added a gui that actually shows something useful that i can build functionality on

--- src/main/java/com/sparta/view/DisplayManager.java
+++ src/main/java/com/sparta/view/DisplayManager.java
@@ -2,14 +2,16 @@ package com.sparta.view;
 
 import com.sparta.controller.AskQuestions;
 
-public class DisplayManager {
+
+import static javafx.application.Application.launch;
+
+public class DisplayManager{
     public static void main(String[] args) {
 
 
 
-        //new GUI(); // if you're not doing anything with gui then turn this off
-
-
+        GUI gui = new GUI(); // if you're not doing anything with gui then turn this off
+        gui.guiRun(args);
 
         AskQuestions testQuestions = new AskQuestions();
         testQuestions.askQuestions();
--- src/main/java/com/sparta/view/GUI.java
+++ src/main/java/com/sparta/view/GUI.java
@@ -1,26 +1,69 @@
 package com.sparta.view;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import static javafx.application.Application.launch;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
 
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 //----------------------------------------------------------------------------------------------
 //  GUI stuff
 //----------------------------------------------------------------------------------------------
 
 public class GUI extends Application{
 
-    public GUI() {
+    public static void guiRun(String[] args) {
         launch();
     }
-
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("Title of the Window");
+    public void start(Stage primaryStage){
+
+        //buttons
+        Button buttonBubble = new Button("BubbleSort");
+        Button buttonMerge = new Button("MergeSort");
+        Button buttonQuick = new Button("QuickSort");
+
+        //creating pane object
+        GridPane gridPane = new GridPane();
+        //size of the pane
+        gridPane.setMinSize(400,200);
+        //setting the border
+        gridPane.setPadding(new Insets(10,10,10,10));
+        //setting vert and hor gaps between columns
+        gridPane.setVgap(5);
+        gridPane.setHgap(5);
+
+        //set grid alignment
+        gridPane.setAlignment(Pos.CENTER);
+
+        //arranging buttons onto grid nodes
+        gridPane.add(buttonBubble, 0,0);
+        gridPane.add(buttonMerge,1,0);
+        gridPane.add(buttonQuick,2,0);
+
+        //button styling
+        buttonBubble.setStyle("-fx-background-color: darkslateblue; -fx-text-fill: white;");
+        buttonQuick.setStyle("-fx-background-color: darkslateblue; -fx-text-fill: white;");
+        buttonMerge.setStyle("-fx-background-color: darkslateblue; -fx-text-fill: white;");
+
+        //pane styling
+        gridPane.setStyle("-fx-background-color: BEIGE;");
+
+        //creating a scene object
+        Scene scene = new Scene(gridPane);
+
+        //window title
+        primaryStage.setTitle("Sort Choice GUI");
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
     }
 
 
