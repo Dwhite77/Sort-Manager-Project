a3c2417b6f05f109287754f9c783a24f8b76ad47
Dwhite77 <dan_white18@live.com>
2021-09-30 11:40:40 +0100

    added in more exceptions for array size as well as the logs for those

--- src/main/java/com/sparta/viewandcontroller/AskQuestions.java
+++ src/main/java/com/sparta/viewandcontroller/AskQuestions.java
@@ -1,5 +1,6 @@
 package com.sparta.viewandcontroller;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 //----------------------------------------------------------------------------------------------
@@ -9,29 +10,29 @@ import java.util.Scanner;
 
 public class AskQuestions {
     public static void askQuestions(){
+        int choice2 =0;
         System.out.println("What type of sort would you like to use? BubbleSort, QuickSort or MergeSort");
         Scanner scn = new Scanner(System.in);
         String choice = scn.next();
         System.out.println("What size array would you like to use?");
-        int choice2 = scn.nextInt();
-//        try{
-//
-//        }catch(){
-//
-//        }
-//            I want to go through this and make it so that the integer input is checked, ill have to do a test of a negative int or something
-
-
-        SortManager newManager = new SortManager();
-        newManager.sortManager(choice2, choice);
-
-        System.out.println("Do you want to do another sort?");
-        String again = scn.next();
-
-        if(again.equals("yes") || again.equals("y")){
+        try{
+            choice2 = scn.nextInt();
+        } catch (InputMismatchException e){
+            System.err.println("Input Error try again");
             askQuestions();
+        }finally{
+
+            SortManager newManager = new SortManager();
+            newManager.sortManager(choice2, choice);
+
+            System.out.println("Do you want to do another sort?");
+            String again = scn.next();
+
+            if(again.equals("yes") || again.equals("y")){
+                askQuestions();
+            }
+            else System.exit(0);
         }
-        else System.exit(0);
 
     }
 
--- src/main/java/com/sparta/viewandcontroller/SortManager.java
+++ src/main/java/com/sparta/viewandcontroller/SortManager.java
@@ -18,42 +18,54 @@ public class SortManager {
         long start, end;
         SortFactory theFactory;
         IntArrayGenerate intArray = new IntArrayGenerate();
-        int[] myArr = intArray.intArrayGen(arraySize);
-        start = nanoTime();
 
-        // if im going to use a gui do I need to add try catch exceptions here?
-        // my gui will hav an input for the size of array so I will catch that and can use what I have here to replace it with
-
-        switch (choice) {
-            case "BubbleSort", "bubblesort", "B", "b", "Bubble", "bubble":
-                theFactory = new BubbleSort();
-                break;
-            case "MergeSort", "mergesort", "merge", "Merge", "M", "m":
-                theFactory = new MergeSort();
-                break;
-            case "QuickSort", "quicksort", "q", "Q", "quick", "Quick":
-                theFactory = new QuickSort();
-                break;
-            default:
-                theFactory = null;
-                break;
-        }
-
-        //int[] mySortedArray = theFactory.sort(myArr);
-        //theFactory.printArray(mySortedArray);
-        try {
-            theFactory.printArray(theFactory.sort(myArr));
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.err.println("Choose an actual Sort!");
+        try{
+            int[] exceptionArr = new int[arraySize];
+        }catch(NegativeArraySizeException e){
+            System.err.println("Negative Array size selected please try to use a positive integer");
             Logger log = LoggingMain.getLogger();
-            log.error("User didn't Select an actual sort");
+            log.error("User didn't Select a positive integer for the Array size");
             AskQuestions.askQuestions();
-        }finally {
+        }
+        //  I want to go through this and make it so that the integer input is checked, ill have to do a test of a negative int or something
+        finally {
+            int[] myArr = intArray.intArrayGen(arraySize);
+            start = nanoTime();
 
-        end = nanoTime();
-        System.out.println("Type of sort used: " + theFactory.toString());
-        System.out.println("Elapsed Time: " + (end - start));
+            // if im going to use a gui do I need to add try catch exceptions here?
+            // my gui will hav an input for the size of array so I will catch that and can use what I have here to replace it with
+
+            switch (choice) {
+                case "BubbleSort", "bubblesort", "B", "b", "Bubble", "bubble":
+                    theFactory = new BubbleSort();
+                    break;
+                case "MergeSort", "mergesort", "merge", "Merge", "M", "m":
+                    theFactory = new MergeSort();
+                    break;
+                case "QuickSort", "quicksort", "q", "Q", "quick", "Quick":
+                    theFactory = new QuickSort();
+                    break;
+                default:
+                    theFactory = null;
+                    break;
+            }
+
+            //int[] mySortedArray = theFactory.sort(myArr);
+            //theFactory.printArray(mySortedArray);
+            try {
+                theFactory.printArray(theFactory.sort(myArr));
+            } catch (Exception e) {
+//                e.printStackTrace();
+                System.err.println("Choose an actual Sort!");
+                Logger log = LoggingMain.getLogger();
+                log.error("User didn't Select an actual sort");
+                AskQuestions.askQuestions();
+            }finally {
+
+            end = nanoTime();
+            System.out.println("Type of sort used: " + theFactory.toString());
+            System.out.println("Elapsed Time: " + (end - start));
+            }
         }
     }
 
